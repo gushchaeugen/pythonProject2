22af521d981e38f673bccb6d4b52febba72aae53
gushchaeugen <tolstiy-06@mail.ru>
2023-02-15 11:02:04 +0300

    hw2

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
+#li = [5]
+#a = [1, 5, 2, 4, 3]
+#b = 1
+#while b != len(a):
+#    if a[b] > a[b - 1]:
+#       print(a[b], end=' ')
+#    b += 1
+
+
+
+#li = [5]
+#a = [1, 5, 2, 4, 3]
+#for i in range(len(a)-1):
+#    n = int(a[i])
+#    i = i + 1
+#    m = int(a[i])
+#    if m > n:
+#        n = m
+#        print(m, end=' ')
+
+
+
+#li = [5]
+#a = [1, -2, 3, -4, 5]
+#n = 0
+#for i in a:
+#    if i > 0:
+#       n += 1
+#print(n)
+
+
+
+#i = [5]
+#a = [1, 5, 2, 4, 3]
+#a = input() .split()
+#for i in range(1,len(a)):
+#    if a [i] > a [i-1]:
+#        print(a[i])
+
+
+
 li = [5]
 a = [1, 5, 2, 4, 3]
 for i in range(1,len(a)):
